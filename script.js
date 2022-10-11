e53007b82b470ccbfcd774420b38f2d9c8d9d5da
Emily Santos <eyssantos@gmail.com>
2022-10-11 11:52:17 +0100

    get data from form

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
-const bookmarkForm = document.getElementById('bookform-form');
+const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl= document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
@@ -16,3 +16,17 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false);
+
+// Handle data from form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+  }
+  console.log(urlValue)
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
