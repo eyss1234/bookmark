e9dc0660454313e95b301827d460d1150f3be944
Emily Santos <eyssantos@gmail.com>
2022-10-11 12:05:35 +0100

    regex validations for email, alert if one of the fields is not submitted

--- script.js
+++ script.js
@@ -17,6 +17,21 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false);
 
+// Validate form
+function validate(nameValue, urlValue) {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if(!nameValue || !urlValue) {
+    alert('Please submit values for both fields');
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  return true;
+}
+
 // Handle data from form
 function storeBookmark(e) {
   e.preventDefault();
@@ -25,7 +40,9 @@ function storeBookmark(e) {
   if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
      urlValue = `https://${urlValue}`; 
   }
-  console.log(urlValue)
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
 }
 
 // Event Listener
