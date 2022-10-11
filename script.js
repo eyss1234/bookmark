1359f8f6f59d9623a280beb1396512a78b2e7884
Emily Santos <eyssantos@gmail.com>
2022-10-11 16:51:03 +0100

    add bookmark function creates a new container with form values

--- script.js
+++ script.js
@@ -34,6 +34,37 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Build bookmarks DOM
+function buildBookmarks() {
+  bookmarks.forEach((bookmark) => {
+    const {name, url} = bookmark;
+    // item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // close icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fa-regular', 'fa-circle-xmark');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onClick', `deleteBookmark('${url}')`);
+    // favicon / link container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`)
+    favicon.setAttribute('alt', 'Favicon');
+    // link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`)
+    link.setAttribute('target','_blank');
+    link.textContent = name;
+    // append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
 // Fetch Bookmarks from local storage
 function fetchBookmarks() {
   if (localStorage.getItem('bookmarks')) {
@@ -45,9 +76,9 @@ function fetchBookmarks() {
         url: 'https://troopl.com/eyssantos',
       }
     ];
-    localStorage.setItem('bookmakrs', JSON.stringify(bookmarks))
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
   }
-  console.log(bookmarks);
+  buildBookmarks();
 }
 
 // Handle data from form
